5ae4900a8c96ab78691f0cf458ae1ae8417ffc00
carlosaltan18 <carlosaltan850@gmail.com>
2025-05-14 19:02:05 -0600

    feat(función de filtro evento) Se agrego una función de filtro de evento

--- api/routes/event.js
+++ api/routes/event.js
@@ -3,16 +3,16 @@ const router = express.Router();
 const eventController = require('../controller/event');
 const checkAuth = require('../middleware/check-auth');
 
-router.post('/', eventController.createEvent);
+router.post('/', checkAuth, eventController.createEvent);
 router.get('/', eventController.getAllEvents);
 router.get('/:id', eventController.getEventById);
-router.put('/:id', eventController.updateEvent);
-router.delete('/:id', eventController.deleteEvent);
+router.put('/:id', checkAuth, eventController.updateEvent);
+router.delete('/:id', checkAuth, eventController.deleteEvent);
 router.get('/filter', eventController.filterEvents);
 
 // Agregar imagen a un evento
-router.post('/:id/images', eventController.addImage);
+router.post('/:id/images', checkAuth, eventController.addImage);
 // Agregar participante a un evento
-router.post('/:id/participants', eventController.addParticipant);
+router.post('/:id/participants', checkAuth, eventController.addParticipant);
 
 module.exports = router;
